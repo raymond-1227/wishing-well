56b2dcee21156bcd1353b36b90f709040581b3af
Raymond Hsu <40461364+raymond-1227@users.noreply.github.com>
2020-10-03 03:19:54 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+// client-side js
+// run by the browser each time your view template is loaded
+
+console.log('Hello, World!');
+
+// the default array of dreams
+const dreams = [
+  'Code a better website',
+  'Make everything free in the world',
+  'Become a billionare'
+];
+
+// define variables that reference elements on the page
+const dreamsList = document.getElementById('dreams');
+const dreamsForm = document.forms[0];
+const dreamInput = dreamsForm.elements['dream'];
+
+// a helper function that creates a list item for a given dream
+const appendNewDream = function(dream) {
+  const newListItem = document.createElement('li');
+  newListItem.innerHTML = dream;
+  dreamsList.appendChild(newListItem);
+}
+
+// iterate through every dream and add it to the page
+dreams.forEach( function(dream) {
+  appendNewDream(dream);
+});
+
+// listen for the form to be submitted and add a new dream when it is
+dreamsForm.onsubmit = function(event) {
+  // stop the form submission from refreshing the page
+  event.preventDefault();
+
+  // get dream value and add it to the list
+  dreams.push(dreamInput.value);
+  appendNewDream(dreamInput.value);
+
+  // reset form 
+  dreamInput.value = '';
+  dreamInput.focus();
+};
